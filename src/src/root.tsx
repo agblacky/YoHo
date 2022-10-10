aa0bbd8c1480af4d87260db1e2ee786710c14089
agblacky <70974889+agblacky@users.noreply.github.com>
2022-10-10 19:59:08 +0200

    Project Reset
    Set up a clean environment

--- src/src/root.tsx
+++ src/src/root.tsx
@@ -1,26 +1,17 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikCity, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
+import { Counter } from './components/counter/counter';
+import { Logo } from './components/logo/logo';
 
-import './global.css';
-
-export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCity> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
+export default () => {
   return (
-    <QwikCity>
+    <>
       <head>
         <meta charSet="utf-8" />
-        <RouterHead />
+        <title>Qwik Blank App</title>
       </head>
-      <body lang="en">
-        <RouterOutlet />
-        <ServiceWorkerRegister />
+      <body>
+        <Logo />
+        <Counter />
       </body>
-    </QwikCity>
+    </>
   );
-});
+};
